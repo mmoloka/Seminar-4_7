151c862836762e2f7c6310e84dc5028308a12dd8
«Mikhail» <kopfgolova@gmail.com>
2022-05-30 20:19:05 +0300

    add Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -5,4 +5,40 @@ m = 3, n = 4.
 5 -2 33 -2
 77 3  8  1
 */
-Console.WriteLine("Hello, World!");
+int GetDemension(string message)
+{
+Console.WriteLine(message);
+int demension = int.Parse(Console.ReadLine());
+return demension;
+}
+
+int [,] InitMatrix(int firstDemension, int secondDemension)
+{
+int [,] matrix = new int[firstDemension, secondDemension];
+Random rnd = new Random();
+for(int i = 0; i < firstDemension; i++)
+{
+for(int j = 0; j < secondDemension; j++)
+matrix[i, j] = rnd.Next(1,20);
+}
+
+return matrix;
+}
+
+void PrintMatrix(int [,] matrix)
+{
+Console.WriteLine("Заполненная матрица");
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+for(int j = 0; j < matrix.GetLength(1); j++)
+Console.Write($"{matrix[i, j]} ");
+
+    Console.WriteLine();
+}
+}
+
+int firstDemension = GetDemension("Введите длину для первого разряда матрицы:");
+int secondDemension = GetDemension("Введите длину для второго разряда матрицы:");
+
+int [,] resultMatrix = InitMatrix(firstDemension, secondDemension);
+PrintMatrix(matrix: resultMatrix);
